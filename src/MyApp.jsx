73d3f667d183364ba7c845ce3f2ae3eaafe76bfa
dsc712 <devyendushekhar712@gmail.com>
2020-01-17 12:51:38 +0530

    add activity progress card and shellbar

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,24 @@
 import React, { useState } from "react";
-import { Card, Text } from "@ui5/webcomponents-react";
 import { spacing } from "@ui5/webcomponents-react-base";
 import { BarChart, LineChart } from "@ui5/webcomponents-react-charts";
+import "@ui5/webcomponents/dist/icons/add.js";
+import {
+  Card,
+  Text,
+  ShellBar,
+  ShellBarItem,
+  List,
+  StandardListItem,
+  ValueState,
+  ProgressIndicator,
+  Title,
+  TitleLevel,
+  FlexBox,
+  FlexBoxJustifyContent,
+  FlexBoxWrap,
+  FlexBoxDirection,
+  AnalyticalTable
+} from "@ui5/webcomponents-react";
 
 export function MyApp() {
   const [activeChart, setActiveChart] = useState("lineChart");
@@ -43,6 +60,13 @@ export function MyApp() {
   ];
   return (
     <div>
+      <ShellBar
+        logo={"reactLogo.png"}
+        profile={"profilePictureExample.png"}
+        primaryTitle="My App"
+      >
+        <ShellBarItem src="sap-icon://add" text="Add" />
+      </ShellBar>
       <Card
         heading="Stock Analysis"
         style={{ width: "300px" }}
@@ -56,6 +80,46 @@ export function MyApp() {
         ) : (
           <BarChart datasets={datasets} labels={labels} loading={loading} />
         )}
+      </Card>
+      <Card heading="Progress" subtitle="List" style={{ width: "300px" }}>
+        <List>
+          <StandardListItem info="finished" infoState={ValueState.Success}>
+            Activity 1
+          </StandardListItem>
+          <StandardListItem info="failed" infoState={ValueState.Error}>
+            Activity 2
+          </StandardListItem>
+          <StandardListItem
+            info="in progress"
+            infoState={ValueState.Warning}
+            style={{ height: "80px" }}
+          >
+            <FlexBox direction={FlexBoxDirection.Column}>
+              <Title level={TitleLevel.H5}>Activity 3</Title>
+              <ProgressIndicator
+                displayValue="89%"
+                percentValue={89}
+                width="180px"
+                state={ValueState.Success}
+              />
+            </FlexBox>
+          </StandardListItem>
+          <StandardListItem
+            info="in progress"
+            infoState={ValueState.Warning}
+            style={{ height: "80px" }}
+          >
+            <FlexBox direction={FlexBoxDirection.Column}>
+              <Title level={TitleLevel.H5}>Activity 4</Title>
+              <ProgressIndicator
+                displayValue="5%"
+                percentValue={5}
+                width="180px"
+                state={ValueState.Error}
+              />
+            </FlexBox>
+          </StandardListItem>
+        </List>
       </Card>
     </div>
   );
